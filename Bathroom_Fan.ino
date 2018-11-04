fdf04d564ddbdfde4fd4c061b0d8593ae7cf2b76
Mark Kozel <github@markkozel.net>
2018-11-04 11:47:45 -0800

    Removed 7-seg display. Cleaned up and added DEBUG blocks.

--- Bathroom_Fan.ino
+++ Bathroom_Fan.ino
@@ -1,51 +1,27 @@
-#include <SPI.h>
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
+
+// Uncomment to see debugging info in Serial Console
+//#define DEBUG 1
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-//#define LOGO16_GLCD_HEIGHT 16 
-//#define LOGO16_GLCD_WIDTH  16 
-//static const unsigned char PROGMEM logo16_glcd_bmp[] =
-//{ B00000000, B11000000,
-//  B00000001, B11000000,
-//  B00000001, B11000000,
-//  B00000011, B11100000,
-//  B11110011, B11100000,
-//  B11111110, B11111000,
-//  B01111110, B11111111,
-//  B00110011, B10011111,
-//  B00011111, B11111100,
-//  B00001101, B01110000,
-//  B00011011, B10100000,
-//  B00111111, B11100000,
-//  B00111111, B11110000,
-//  B01111100, B11110000,
-//  B01110000, B01110000,
-//  B00000000, B00110000 };
-
+// elements for OLED Display
 char displayString[5];
 boolean keepAliveOn = false;
 boolean updateDisplay = false;
 
+// Elements for Add Minute push button
 const int BUTTON_Pin = 2;
 volatile boolean keyPressed = false;
-
-// variable used for the debounce
 unsigned long timeNewKeyPress = 0;
 unsigned long timeLastKeyPress = 0;
-unsigned int timeDebounce = 100;
+unsigned int timeDebounce = 250;
 
-const int LED_Pin = 7;
-// variable used to control the LED
-boolean LEDstatus = LOW;
-
-//int buttonCurrentState = 0;
-//int buttonLastState = 0;
-
-const int FAN_Pin = 3; //Fan control index
+const int FAN_Pin = 3; // Fan control index
 int fanCurrentState = 0;
 const int FAN_UP = 0;
 const int FAN_DOWN = 1;
@@ -53,48 +29,32 @@ const int MAX_FAN_DC = 79;
 
 // const int led_pin = PB5; //Digital Pin 13 and on-board led
 const uint16_t t1_load = 0;
-const uint16_t t1_comp = 62245; //Compare value 256 -  (1s * 16MHz)/256
+const uint16_t t1_comp = 62245; // Compare value 256 -  (1s * 16MHz)/256
 
 int minuteCount = 0;
 int countdownSeconds = 0;
-const int MINUTE_COUNT_MAX = 15;
-
-
-//For 7-segment LED display
-#include <ShiftRegister74HC595.h>
-int dataPin = 4;
-int latchPin = 5;
-int clockPin = 6;
-// create shift register object (number of shift registers, data pin, clock pin, latch pin)
-ShiftRegister74HC595 sr (1, dataPin, clockPin, latchPin);
-
-boolean buttonWasPressed;
+const int MINUTE_COUNT_MAX = 30;
 
 void setup()
 {
   Serial.begin(115200);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.begin(SSD1306_SWITCHCAPVCC,
+                0x3C); // initialize with the I2C addr 0x3C (for the 128x32)
   display.clearDisplay();
   display.drawPixel(10, 10, WHITE);
   display.display();
 
   UpdateOLEDDisplay(0);
-  
-  //myfnUpdateDisplay(myfnNumToBits(-1));
 
-  //Setup fan and init PWM on
+  // Setup fan and init PWM on
   pinMode(FAN_Pin, OUTPUT);
   pwm25kHzBegin();
   pwmDuty(0);
 
   pinMode(BUTTON_Pin, INPUT);
-  //digitalWrite(BUTTON_Pin, LOW);
-  attachInterrupt( digitalPinToInterrupt(BUTTON_Pin), keyIsPressed, RISING );
+  attachInterrupt(digitalPinToInterrupt(BUTTON_Pin), keyIsPressed, FALLING);
 
-  pinMode(LED_Pin, OUTPUT);  
-   digitalWrite(LED_Pin,LOW); 
-   
   initTimer1();
 }
 
@@ -102,103 +62,97 @@ void loop()
 {
   if (keyPressed)
   {
-      keyPressed = false;
-      timeNewKeyPress = millis();
-  
-      if ( timeNewKeyPress - timeLastKeyPress >= timeDebounce)
-      {
-          Serial.print("button: ");
-          Serial.println(digitalRead(BUTTON_Pin));
-          addMinuteToTime();
-          blink();
-      }
-      timeLastKeyPress = timeNewKeyPress;
+    keyPressed = false;
+    timeNewKeyPress = millis();
+
+    if (timeNewKeyPress - timeLastKeyPress >= timeDebounce)
+    {
+#ifdef DEBUG
+      Serial.print("button: ");
+      Serial.println(digitalRead(BUTTON_Pin));
+#endif
+      addMinuteToTime();
+    }
+    timeLastKeyPress = timeNewKeyPress;
   }
 
-  if(updateDisplay){
+  if (updateDisplay)
+  {
     display.display();
     updateDisplay = false;
   }
 }
 
-void blink()
-{
-      if (LEDstatus == LOW) { LEDstatus = HIGH; } else { LEDstatus = LOW; }   
-      digitalWrite(LED_Pin, LEDstatus);
-}
+// ISR for button press. Sets flag and leaves
+void keyIsPressed() { keyPressed = true; }
 
-//ISR for button press
-//Sets flag and leaves
-void keyIsPressed()
-{
-   keyPressed = true;
-}
-
-
+// Checks for end of timer to turn off fan.
+// Decrements second and minute (if needed) counters
+// Updates display if minute counter changed
+// Called from 1-second ISR
 int checkForTimesUp()
-{   
+{
   if (countdownSeconds > 0)
-  { //Still time. Count down and check for minute mark
+  { // Still time. Count down and check for minute mark
     countdownSeconds--;
 
     if ((countdownSeconds % 60) == 0)
     {
       minuteCount--;
-       //myfnUpdateDisplay(myfnNumToBits(minuteCount));
-       UpdateOLEDDisplay(minuteCount);
+      UpdateOLEDDisplay(minuteCount);
     }
-
+#ifdef DEBUG
     Serial.print("   countdownSeconds: ");
     Serial.println(countdownSeconds);
 
     Serial.print("   minuteCount: ");
     Serial.println(minuteCount);
-
-//    toggleKeepAlive();
+#endif
   }
   else
-  { //Zero seconds. Clean up and turn off fan
+  { // Zero seconds. Clean up and turn off fan
     countdownSeconds = 0;
     minuteCount = 0;
-
+#ifdef DEBUG
     Serial.print("   countdownSeconds: ");
     Serial.println(countdownSeconds);
 
     Serial.print("   minuteCount: ");
     Serial.println(minuteCount);
+#endif
 
-    //toggleKeepAlive();
-
-     //myfnUpdateDisplay(myfnNumToBits(minuteCount));
-     UpdateOLEDDisplay(minuteCount);
+    UpdateOLEDDisplay(minuteCount);
 
     if (fanCurrentState != 0)
     {
       fanCurrentState = 0;
-      pwmDuty(fanCurrentState); //Turn off fan
+      pwmDuty(fanCurrentState); // Turn off fan
     }
   }
 }
 
+// Called when button is pressed. Adds 1 minute unless already at
+// MINUTE_COUNT_MAX
 void addMinuteToTime()
 {
+#ifdef DEBUG
   Serial.println("addMinuteToTime");
+#endif
   if (minuteCount < MINUTE_COUNT_MAX)
   {
-    countdownSeconds = countdownSeconds + 60; //convert to seconds
-    minuteCount = countdownSeconds / 60;
-//    Num_Write(minuteCount);
-    //myfnUpdateDisplay(myfnNumToBits(minuteCount));
+    countdownSeconds = countdownSeconds + 60; // convert to seconds
+    minuteCount++;
     UpdateOLEDDisplay(minuteCount);
-
+#ifdef DEBUG
     Serial.print("countdownSeconds is now ");
     Serial.println(countdownSeconds);
     Serial.print("minuteCount is now ");
     Serial.println(minuteCount);
-
+#endif
     fanCurrentState = MAX_FAN_DC;
-    pwmDuty(fanCurrentState); //Turn on fan
+    pwmDuty(fanCurrentState); // Turn on fan
   }
+#ifdef DEBUG
   else
   {
     Serial.print("minuteCount already maxed out at ");
@@ -206,49 +160,57 @@ void addMinuteToTime()
     Serial.print("Max Value is  ");
     Serial.println(MINUTE_COUNT_MAX);
   }
+#endif
 }
 
+// Setup TC2 for PWM Fan Control
 void pwm25kHzBegin()
 {
-  TCCR2A = 0;                                            // TC2 Control Register A
-  TCCR2B = 0;                                            // TC2 Control Register B
-  TIMSK2 = 0;                                            // TC2 Interrupt Mask Register
-  TIFR2 = 0;                                             // TC2 Interrupt Flag Register
-  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20); // OC2B cleared/set on match when up/down counting, fast PWM
-  TCCR2B |= (1 << WGM22) | (1 << CS21);                  // prescaler 8
-  OCR2A = 79;                                            // TOP overflow value (Hz)
+  TCCR2A = 0; // TC2 Control Register A
+  TCCR2B = 0; // TC2 Control Register B
+  TIMSK2 = 0; // TC2 Interrupt Mask Register
+  TIFR2 = 0;  // TC2 Interrupt Flag Register
+  TCCR2A |=
+      (1 << COM2B1) | (1 << WGM21) |
+      (1 << WGM20);                     // OC2B cleared/set on match when up/down counting, fast PWM
+  TCCR2B |= (1 << WGM22) | (1 << CS21); // prescaler 8
+  OCR2A = 79;                           // TOP overflow value (Hz)
   OCR2B = 0;
 }
 
+// Set/Change PWM Fan duty cycle
 void pwmDuty(byte ocrb)
 {
   OCR2B = ocrb; // PWM Width (duty)
+#ifdef DEBUG
   Serial.print("Fan ordered to ");
   Serial.println(ocrb);
+#endif
 }
 
+// Setup timer for 1-second interrupt
 void initTimer1()
 {
-  //Reset Time1 Ctrl Reg A to default
+  // Reset Time1 Ctrl Reg A to default
   TCCR1A = 0;
 
-  //Set prescalar to 256 for 1 second timer (b100)
+  // Set prescalar to 256 for 1 second timer (b100)
   TCCR1B |= (1 << CS12);
   TCCR1B &= ~(1 << CS11);
   TCCR1B &= ~(1 << CS10);
 
-  //ResetTimer1 and set compare value
+  // ResetTimer1 and set compare value
   TCNT1 = t1_load;
   OCR1A = t1_comp;
 
-  //Set Timer1 compare interrupt enable
+  // Set Timer1 compare interrupt enable
   TIMSK1 = (1 << OCIE1A);
 
-  //Enable global inettupts
+  // Enable global inettupts
   sei();
 }
 
-//ISA for 1 second check/keep alive
+// ISA for 1-second check
 ISR(TIMER1_COMPA_vect)
 {
   checkForTimesUp();
@@ -256,96 +218,39 @@ ISR(TIMER1_COMPA_vect)
   TCNT1 = t1_load;
 }
 
-void toggleKeepAlive() {
+// Update Keep Alive graphic. Wait for 1-second interrupt to update display
+void toggleKeepAlive()
+{
   uint8_t Toggle_Color;
-  
-  if(keepAliveOn) {
+
+  if (keepAliveOn)
+  {
     Toggle_Color = BLACK;
     keepAliveOn = false;
-  } else {
+  }
+  else
+  {
     Toggle_Color = WHITE;
     keepAliveOn = true;
   }
-    display.fillRoundRect(123, 27, 5, 5, 1, Toggle_Color);
-    
-    updateDisplay = true;
-}
-
-void UpdateOLEDDisplay(int time) {
-  sprintf(displayString, "%d min", time);
-  Serial.print("displayString() => ");
-  Serial.println(displayString);
-
-  display.clearDisplay();
-  display.setTextSize(4);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println(displayString);
+  display.fillRoundRect(123, 27, 5, 5, 1, Toggle_Color);
 
   updateDisplay = true;
 }
 
-//void myfnUpdateDisplay(byte eightBits) {
-//  digitalWrite(latchPin, LOW);  // prepare shift register for data
-//  shiftOut(dataPin, clockPin, LSBFIRST, eightBits); // send data
-//  digitalWrite(latchPin, HIGH); // update display
-//}
-//
-//byte myfnNumToBits(int someNumber) {
-//  switch (someNumber) {
-//    case -1: //Clear
-//      return B00000000;
-//      break;
-//    case 0:
-//      return B11111100;
-//      break;
-//    case 1:
-//      return B01100000;
-//      break;
-//    case 2:
-//      return B11011010;
-//      break;
-//    case 3:
-//      return B11110010;
-//      break;
-//    case 4:
-//      return B01100110;
-//      break;
-//    case 5:
-//      return B10110110;
-//      break;
-//    case 6:
-//      return B10111110;
-//      break;
-//    case 7:
-//      return B11100000;
-//      break;
-//    case 8:
-//      return B11111110;
-//      break;
-//    case 9:
-//      return B11110110;
-//      break;
-//    case 10:
-//      return B11101110; // Hexidecimal A
-//      break;
-//    case 11:
-//      return B00111110; // Hexidecimal B
-//      break;
-//    case 12:
-//      return B10011100; // Hexidecimal C
-//      break;
-//    case 13:
-//      return B01111010; // Hexidecimal D
-//      break;
-//    case 14:
-//      return B10011110; // Hexidecimal E
-//      break;
-//    case 15:
-//      return B10001110; // Hexidecimal F
-//      break;  
-//    default:
-//      return B10010010; // Error condition, displays three vertical bars
-//      break;   
-//  }
-//}
+// Update timer on diplay. Wait for 1-second interrupt to update display
+void UpdateOLEDDisplay(int time)
+{
+  sprintf(displayString, "%d min", time);
+#ifdef DEBUG
+  Serial.print("displayString() => ");
+  Serial.println(displayString);
+#endif
+  display.clearDisplay();
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(displayString);
+
+  updateDisplay = true;
+}
